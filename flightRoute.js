d83f874d7472d989dd6acd4d503451e210548915
Chimmezirim ibekwe <98124045+Mmezirim@users.noreply.github.com>
2022-07-01 19:54:16 +0100

    Update flightRoute.js

--- flightRoute.js
+++ flightRoute.js
@@ -4,6 +4,12 @@ const router = express.Router();
 const controller = require('../controllers/flightController');
 
 router.get('/', controller.example)
+router.post('/', controller.newFLight)
+router.get('/', controller.allFLight)
+router.get('/', controller.singleFlight)
+router.patch('/:id', controller.updateFlight)
+router.delete('/', controller.deleteFlight)
+
 
 module.exports = router;
 
